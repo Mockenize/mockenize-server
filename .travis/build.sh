16d8e4bf40342a626d10b0a9569b28dd295d8c6b
Rodrigo Seidi Watanabe <rseidi00@gmail.com>
2016-02-12 01:25:28 -0200

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,7 +3,9 @@
 VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
 
 if [[ $VERSION == *"SNAPSHOT"* ]]; then
+	echo "Building SNAPSHOT"
 	mvn compile test
 else
+	echo "Building RELEASE"
 	mvn deploy --settings .travis/maven-settings.xml
 fi
